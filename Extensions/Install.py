074f824815bc5eca4e4f7c2e8df649dd1f3762ba
sigvetj <sigvetj@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2002-08-30 07:08:34 +0000

    Added portal_navigation properties.

--- Extensions/Install.py
+++ Extensions/Install.py
@@ -38,4 +38,32 @@ def install(self):
         else:
             out.write('Skipping %s skin\n' % skin)
 
+#    portal_form=getToolByName(self, 'portal_form')
+#    portal_form.setValidators('photo_edit_form', ['validate_id','validate_photo_edit'])
+
+    portal_prop=getToolByName(self, 'portal_properties')
+    nav=portal_prop.navigation_properties
+
+    nav.manage_addProperty('photo.image_edit.success','action:view','string');
+    nav.manage_addProperty('photo.image_edit.failure','action:edit','string');
+    nav.manage_addProperty('photo.image_edit_form.success','script:image_edit','string');
+    nav.manage_addProperty('photo.image_edit_form.failure','image_edit_form','string');
+
+    nav.manage_addProperty('default.photo_settings.succes','action:view','string');
+    nav.manage_addProperty('default.photo_settings.failure','action:photo_settings','string');
+    nav.manage_addProperty('default.photo_settings_form.succes','script:photo_settings','string');
+    nav.manage_addProperty('default.photo_settings_form.failure','photo_settings_form','string');
+
+    nav.manage_addProperty('default.photo_rotate.succes','action:view','string');
+    nav.manage_addProperty('default.photo_rotate.failure','action:photo_rotate','string');
+    nav.manage_addProperty('default.photo_rotate_form.succes','script:photo_rotate','string');
+    nav.manage_addProperty('default.photo_rotate_form.failure','photo_rotate_form','string');
+
+    nav.manage_addProperty('photofolder.folder_edit_form.success','script:folder_edit','string');
+    nav.manage_addProperty('photofolder.folder_edit_form.failure','folder_edit_form','string');
+    nav.manage_addProperty('photofolder.folder_edit.success','action:view','string');
+    nav.manage_addProperty('photofolder.folder_edit.failure','action:edit','string');
+
+    out.write('Added navigation properties.')
+
     return out.getvalue()
