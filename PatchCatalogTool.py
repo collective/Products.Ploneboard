485c8a7131542bb00b1b7d70121371081c1cf2e0
vladoi <vladoi@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2003-07-10 13:33:33 +0000

    added try/except around the import of CMFCore.CatalogTool

--- PatchCatalogTool.py
+++ PatchCatalogTool.py
@@ -1,20 +1,23 @@
 """
-$Id: PatchCatalogTool.py,v 1.1 2003/07/10 10:20:32 vladoi Exp $
+$Id: PatchCatalogTool.py,v 1.2 2003/07/10 13:33:33 vladoi Exp $
 """
 
-from zLOG import LOG, INFO
-from Products.CMFCore.CatalogTool import CatalogTool
+try:
+    from Products.CMFCore.CatalogTool import CatalogTool
+except ImportError:
+    pass
+else:
+    if not hasattr(CatalogTool, '_old_listAllowedRolesAndUsers'):
+        def _listAllowedRolesAndUsers(self, user):
+            result = self._old_listAllowedRolesAndUsers(user)
+            getGroups = getattr(user, 'getGroups', None)
+            if getGroups is not None:
+                for group in getGroups():
+                    result.append('user:'+group)
+            return result
 
-if not hasattr(CatalogTool, '_old_listAllowedRolesAndUsers'):
-    def _listAllowedRolesAndUsers(self, user):
-        result = self._old_listAllowedRolesAndUsers(user)
-        getGroups = getattr(user, 'getGroups', None)
-        if getGroups is not None:
-            for group in getGroups():
-                result.append('user:'+group)
-        return result
+        from zLOG import LOG, INFO
+        LOG('GroupUserFolder', INFO, 'Patching CatalogTool')
 
-    LOG('GroupUserFolder', INFO, 'Patching CatalogTool')
-
-    CatalogTool._old_listAllowedRolesAndUsers = CatalogTool._listAllowedRolesAndUsers
-    CatalogTool._listAllowedRolesAndUsers = _listAllowedRolesAndUsers
+        CatalogTool._old_listAllowedRolesAndUsers = CatalogTool._listAllowedRolesAndUsers
+        CatalogTool._listAllowedRolesAndUsers = _listAllowedRolesAndUsers
