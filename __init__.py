0b0cf5eb01daab627339167cba9ff7f2f85dc283
limi <limi@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2003-09-23 20:05:39 +0000

    import statements for Validators were missing.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-# $Id: __init__.py,v 1.3 2003/09/23 17:57:56 plonista Exp $
+# $Id: __init__.py,v 1.4 2003/09/23 20:05:39 limi Exp $
 # $Source: /home/hazmat/projects/psvn/collective/CMFFormController/__init__.py,v $
-__version__ = "$Revision: 1.3 $"[11:-2]
+__version__ = "$Revision: 1.4 $"[11:-2]
 """Initialize CMFFormController"""
 
 import sys
@@ -10,6 +10,7 @@ from Products.CMFCore.utils import registerIcon, ToolInit
 from config import *
 import ControllerPageTemplate, FSControllerPageTemplate
 import ControllerPythonScript, FSControllerPythonScript
+import ControllerValidator, FSControllerValidator
 import FormController
 from Actions import RedirectTo, TraverseTo, RedirectToAction, TraverseToAction
 
@@ -33,7 +34,16 @@ def initialize(context):
                       ControllerPythonScript.manage_addControllerPythonScript),
         icon='www/cpy.gif',
         )
+    context.registerClass(
+        ControllerValidator.ControllerValidator,
+        constructors=(ControllerValidator.manage_addControllerValidatorForm,
+                      ControllerValidator.manage_addControllerValidator),
+        icon='www/vpy.gif',
+        )
     registerIcon(FSControllerPageTemplate.FSControllerPageTemplate,
                  'www/cpt.gif', globals())
     registerIcon(FSControllerPythonScript.FSControllerPythonScript,
                  'www/cpy.gif', globals())
+    registerIcon(FSControllerValidator.FSControllerValidator,
+                 'www/vpy.gif', globals())
+
