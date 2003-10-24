6968656546bbb3e11cc165d68a340e443eb08bef
k_vertigo <k_vertigo@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2003-10-24 13:03:06 +0000

    New repository initialized by cvs2svn.

--- BannerPermissions.py
+++
@@ -1,6 +0,0 @@
-from Products.CMFCore.CMFCorePermissions import *
-
-bannerManager = 'Banner manager'
-
-ManageBanners = 'Manage banners'
-setDefaultRoles(ManageBanners, ('Manager', bannerManager))
--- config.py
+++
@@ -1,9 +0,0 @@
-from Products.CMFCore.CMFCorePermissions import AddPortalContent
-
-ADD_CONTENT_PERMISSION = AddPortalContent
-PROJECTNAME = "PloneBannerManager"
-SKINS_DIR = 'skins'
-
-GLOBALS = globals()
-
-BANNER_THUMB_SIZE = (117,50)
