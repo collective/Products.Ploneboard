2342fea2afde405c09e052e88588443cc51333f5
k_vertigo <k_vertigo@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2003-12-09 23:47:12 +0000

    New repository initialized by cvs2svn.

--- Namespace.py
+++
@@ -1,6 +0,0 @@
-MetadataNamespace = 'http://www.infrae.com/xml/metadata'
-BindingRunTime = 'http://www.infrae.com/xml/metadata/binding'
-DefaultPrefix = 'example'
-DefaultNamespace = "http://www.example.com/unknown_namespace"
-DublinCore = 'http://www.dublincore.org'
-
