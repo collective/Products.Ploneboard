cdf0fedad2ebb4146da5454a06c192a7bc334194
tiran <tiran@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2003-12-22 16:38:46 +0000

    add msgfmt script to compile po files on the fly (taken from plone-2.3.2/Tools/i18n and modified)

---
+++ msgfmt.py
@@ -0,0 +1,160 @@
+#! /usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+# Written by Martin v. Löwis <loewis@informatik.hu-berlin.de>
+#
+# modified by Christian Heimes <heimes@faho.rwth-aachen.de> for the
+# Placeless Translation Service
+
+"""Generate binary message catalog from textual translation description.
+
+This program converts a textual Uniforum-style message catalog (.po file) into
+a binary GNU catalog (.mo file).  This is essentially the same function as the
+GNU msgfmt program, however, it is a simpler implementation.
+
+This file was taken from Python-2.3.2/Tools/i18n and altered in several ways. 
+Now you can simply use it from another python module:
+
+  from msgfmt import make
+  po = open('mypofile.po')
+  mo = make(po)
+
+where po is a file handler to a po file and mo is the compiled mo file as string.
+
+Exceptions:
+
+  * IOError if the file couldn't be read
+  
+  * PoSyntaxError if the po file has syntax errors
+
+"""
+
+import struct
+import array
+
+__version__ = "1.1pts"
+
+MESSAGES = {}
+
+class PoSyntaxError(Exception):
+    """ Syntax error in a po file """
+    def __init__(self, lno):
+        self.lno = lno
+        
+    def __str__(self):
+        return 'Po file syntax error on line %d' % self.lno
+
+
+def add(id, str, fuzzy):
+    "Add a non-fuzzy translation to the dictionary."
+    global MESSAGES
+    if not fuzzy and str:
+        MESSAGES[id] = str
+
+
+
+def generate():
+    "Return the generated output."
+    global MESSAGES
+    keys = MESSAGES.keys()
+    # the keys are sorted in the .mo file
+    keys.sort()
+    offsets = []
+    ids = strs = ''
+    for id in keys:
+        # For each string, we need size and file offset.  Each string is NUL
+        # terminated; the NUL does not count into the size.
+        offsets.append((len(ids), len(id), len(strs), len(MESSAGES[id])))
+        ids += id + '\0'
+        strs += MESSAGES[id] + '\0'
+    output = ''
+    # The header is 7 32-bit unsigned integers.  We don't use hash tables, so
+    # the keys start right after the index tables.
+    # translated string.
+    keystart = 7*4+16*len(keys)
+    # and the values start after the keys
+    valuestart = keystart + len(ids)
+    koffsets = []
+    voffsets = []
+    # The string table first has the list of keys, then the list of values.
+    # Each entry has first the size of the string, then the file offset.
+    for o1, l1, o2, l2 in offsets:
+        koffsets += [l1, o1+keystart]
+        voffsets += [l2, o2+valuestart]
+    offsets = koffsets + voffsets
+    output = struct.pack("Iiiiiii",
+                         0x950412deL,       # Magic
+                         0,                 # Version
+                         len(keys),         # # of entries
+                         7*4,               # start of key index
+                         7*4+len(keys)*8,   # start of value index
+                         0, 0)              # size and offset of hash table
+    output += array.array("i", offsets).tostring()
+    output += ids
+    output += strs
+    return output
+
+
+
+def make(infile):
+    ID = 1
+    STR = 2
+
+    # rewind to make shure we start from the beginning
+    infile.seek(0)
+
+    #try:
+    # XXX this could raise an IOError
+    lines = open(infile).readlines()
+    #except IOError, msg:
+    #    do something
+    
+    section = None
+    fuzzy = 0
+
+    # Parse the catalog
+    lno = 0
+    for l in lines:
+        lno += 1
+        # If we get a comment line after a msgstr, this is a new entry
+        if l[0] == '#' and section == STR:
+            add(msgid, msgstr, fuzzy)
+            section = None
+            fuzzy = 0
+        # Record a fuzzy mark
+        if l[:2] == '#,' and l.find('fuzzy'):
+            fuzzy = 1
+        # Skip comments
+        if l[0] == '#':
+            continue
+        # Now we are in a msgid section, output previous section
+        if l.startswith('msgid'):
+            if section == STR:
+                add(msgid, msgstr, fuzzy)
+            section = ID
+            l = l[5:]
+            msgid = msgstr = ''
+        # Now we are in a msgstr section
+        elif l.startswith('msgstr'):
+            section = STR
+            l = l[6:]
+        # Skip empty lines
+        l = l.strip()
+        if not l:
+            continue
+        # XXX: Does this always follow Python escape semantics?
+        l = eval(l)
+        if section == ID:
+            msgid += l
+        elif section == STR:
+            msgstr += l
+        else:
+            raise PoSyntaxError(lno)
+    # Add last entry
+    if section == STR:
+        add(msgid, msgstr, fuzzy)
+
+    # Compute output
+    return generate()
+                      
+
+
