64fc8646b57bc205b2de6b9263f0ba441484477f
k_vertigo <k_vertigo@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2004-02-08 19:28:18 +0000

    - new icons and icon registration

--- __init__.py
+++ __init__.py
@@ -2,19 +2,21 @@
 Author: kapil thangavelu <k_vertigo@objectrealms.net>
 """
 
+from Products.PythonScripts.Utility import allow_module
+from Globals import ImageFile, package_home  
+
 import Configuration
 import Compatibility
-
 import Collection
 import Set
 import Element
 import Binding
 import MetadataTool
 
-from Globals import ImageFile
+# save our fs path for install methods
+ProductHome = package_home(globals())
 
 # Allow Errors to be imported TTW
-from Products.PythonScripts.Utility import allow_module
 allow_module('Products.CMFMetadata.Exceptions')
 
 misc_ = {
@@ -22,5 +24,10 @@ misc_ = {
     'down'   : ImageFile('www/down.gif', globals()),
     'top'    : ImageFile('www/top.gif', globals()),
     'bottom' : ImageFile('www/bottom.gif', globals()),
+    'metadatatool.png': ImageFile('www/metadatatool.png', globals()),
+    'metadataset.png': ImageFile('www/metadataset.png', globals()),
+    'typemapping.png': ImageFile('www/typemapping.png', globals()),
+    'collection.png': ImageFile('www/collection.png', globals()),
+    'type.png': ImageFile('www/type.png', globals())
     }
-
+                                                                                
