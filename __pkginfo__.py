55e9a0d73c7b2dcd4161cc47277fa9b5a9cfafd6
tiran <tiran@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2004-04-04 21:49:30 +0000

    increased version number to alpha4

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -1,10 +1,10 @@
 
 modname     = 'ATContentTypes'
-version     = '0.2-alpha2'
+version     = '0.2-alpha4'
 # (major, minor, patchlevel, release info) where release info is:
 # -99 for alpha, -49 for beta, -19 for rc and 0 for final
 # increment the release info number by one e.g. -98 for alpha2
-numversion  = (0, 2, 0, -98)
+numversion  = (0, 2, 0, -96)
 
 license     = 'GPL, ZPL'
 copyright   = '''(c) 2003-2004 AT Content Types development team'''
