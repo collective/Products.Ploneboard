ca6a5bef6a69108245ec07ef926dd9d585631463
panjunyong <panjunyong@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2004-04-19 08:47:25 +0000

    windows pot path comes from registery now. thanks Joachim Bauch

--- config.py
+++ config.py
@@ -13,3 +13,22 @@ DOT_EXE = 'dot'
 
 if os.name == 'nt':
     DOT_EXE = 'dot.exe'
+
+    # patch from Joachim Bauch bauch@struktur.de
+    # on Windows, the path to the ATT Graphviz installation 
+    # is read from the registry. 
+
+    import win32api, win32con
+    # make sure that "key" is defined in our except block
+    key = None
+    try:
+        key = win32api.RegOpenKeyEx(win32con.HKEY_LOCAL_MACHINE, r'SOFTWARE\ATT\Graphviz')
+        value, type = win32api.RegQueryValueEx(key, 'InstallPath')
+        bin_search_path = [os.path.join(str(value), 'bin')]
+    except:
+        if key: win32api.RegCloseKey(key)
+        # key doesn't exist
+        pass
+except ImportError:
+    # win32 may be not installed...
+    pass
