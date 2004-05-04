c83e1714f7974aeb748dec75ae6304c6fab22899
tiran <tiran@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2004-05-04 19:02:28 +0000

    Preparing 0.2-beta4

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -1,10 +1,10 @@
 
 modname     = 'ATContentTypes'
-version     = '0.2-beta3'
+version     = '0.2-beta4'
 # (major, minor, patchlevel, release info) where release info is:
 # -99 for alpha, -49 for beta, -19 for rc and 0 for final
 # increment the release info number by one e.g. -98 for alpha2
-numversion  = (0, 2, 0, -47)
+numversion  = (0, 2, 0, -46)
 
 license     = 'GPL, ZPL'
 copyright   = '''(c) 2003-2004 AT Content Types development team'''
@@ -30,7 +30,7 @@ debian_maintainer_email = ''
 debian_add_descr  = ''''''
 debian_handler    = 'zope'
 debian_builddep   = ''
-debian_depends    = 'zope (>= 2.6), archetypes (>= 1.2.5), zope-cmfplone (>= 2.0)'
+debian_depends    = 'zope (>= 2.6), archetypes (>= 1.3), zope-cmfplone (>= 2.0)'
 debian_recommends = ''
 debian_suggests   = ''
 debian_conflicts  = ''
