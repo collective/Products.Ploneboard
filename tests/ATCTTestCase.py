adf89d5c5118789877d92e430702c73f8624c439
tiran <tiran@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2004-06-06 15:36:33 +0000

    test for mutable dc

--- tests/ATCTTestCase.py
+++ tests/ATCTTestCase.py
@@ -1,6 +1,6 @@
 """ATContentTypes tests
 
-$Id: ATCTTestCase.py,v 1.4 2004/06/06 15:26:15 tiran Exp $
+$Id: ATCTTestCase.py,v 1.5 2004/06/06 15:36:33 tiran Exp $
 """
 
 __author__ = 'Christian Heimes'
@@ -10,12 +10,14 @@ from Testing import ZopeTestCase
 
 from Products.ATContentTypes.interfaces.IATContentType import IATContentType
 from Products.CMFCore.interfaces.DublinCore import DublinCore as IDublinCore
+from Products.CMFCore.interfaces.DublinCore import MutableDublinCore as IMutableDublinCore
 
 class ATCTTestCase(ZopeTestCase.ZopeTestCase):
     """ ATContentTypes test case based on ZopeTestCase"""
 
     def testDoesImplemendDC(self):
         self.failUnless(IDublinCore.isImplementedBy(self._dummy))
+        self.failUnless(IMutableDublinCore.isImplementedBy(self._dummy))
         
     def testDoesImplementATCT(self):
         self.failUnless(IATContentType.isImplementedBy(self._dummy))
