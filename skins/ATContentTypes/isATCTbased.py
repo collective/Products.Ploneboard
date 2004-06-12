461182b02e2f257ab6f84e7a2faedde2e74b151f
tiran <tiran@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2004-06-12 23:38:24 +0000

    Added interface test for objects to be ATCT based

---
+++ skins/ATContentTypes/isATCTbased.py
@@ -0,0 +1,15 @@
+## Script (Python) "isATCTbased"
+##title=Formats the history diff
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=obj
+
+from Products.CMFCore.utils import getToolByName
+
+iface = getToolByName(context, 'portal_interface')
+
+return iface.objectImplements(obj, 
+           'Products.ATContentTypes.interfaces.IATContentType.IATContentType')
