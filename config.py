2f6dee805676c48346165d46e977e7c2cec6c78d
k_vertigo <k_vertigo@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2004-06-30 14:46:05 +0000

    New repository initialized by cvs2svn.

--- config.py
+++
@@ -1,7 +0,0 @@
-from Products.CMFCore.CMFCorePermissions import AddPortalContent
-
-ADD_CONTENT_PERMISSION = AddPortalContent
-PROJECT_NAME = 'ATAmazon'
-SKINS_DIR = 'skins'
-
-GLOBALS = globals()
--- utils.py
+++
@@ -1,8 +0,0 @@
-def toAscii(s):
-    if type(s) != type(u''):
-        s = str(s)
-    snew = ''
-    for c in s:
-        if ord(c) < 128:
-            snew += c
-    return str(snew)
