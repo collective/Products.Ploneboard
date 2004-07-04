24f236514ae0ae2201ed03a5847706d45620004e
limi <limi@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2004-07-04 14:05:12 +0000

    Oops, forgot to change one of the thumb->thumbnail references.

--- types/schemata.py
+++ types/schemata.py
@@ -18,7 +18,7 @@
 #
 """
 
-$Id: schemata.py,v 1.38 2004/07/04 01:05:21 limi Exp $
+$Id: schemata.py,v 1.39 2004/07/04 14:05:12 limi Exp $
 """ 
 __author__  = ''
 __docformat__ = 'restructuredtext'
@@ -292,11 +292,11 @@ ATExtImageSchema = ATContentTypeSchema + Schema((
                primary=1,
                languageIndependent=True,
                storage=ExternalStorage(prefix='atct', archive=False),
-               sizes= {'preview' : (400, 400),
-                       'thumb'   : (128, 128),
-                       'tile'    :  (64, 64),
-                       'icon'    :  (32, 32),
-                       'listing' :  (16, 16),
+               sizes= {'preview'   : (400, 400),
+                       'thumbnail' : (128, 128),
+                       'tile'      :  (64, 64),
+                       'icon'      :  (32, 32),
+                       'listing'   :  (16, 16),
                       },
                validators = MaxSizeValidator('checkFileMaxSize', maxsize=MAX_IMAGE_SIZE),
                widget = ImageWidget(description = "Select the image to be added by clicking the 'Browse' button.",
