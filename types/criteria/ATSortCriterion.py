be8da07dd37484e406785f5b05021065935d4bab
godchap <godchap@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2004-07-19 12:01:50 +0000

    cosmetics

--- types/criteria/ATSortCriterion.py
+++ types/criteria/ATSortCriterion.py
@@ -16,7 +16,7 @@
 ##############################################################################
 """ Topic:
 
-$Id: ATSortCriterion.py,v 1.10 2004/07/19 11:57:36 godchap Exp $
+$Id: ATSortCriterion.py,v 1.11 2004/07/19 12:01:50 godchap Exp $
 """
 
 __author__  = 'Christian Heimes'
@@ -29,7 +29,6 @@ from AccessControl import ClassSecurityInfo
 from Products.ATContentTypes.config import *
 from Products.ATContentTypes.types.criteria import registerCriterion, \
     SORT_INDICES
-from Products.ATContentTypes.Permissions import ChangeTopics
 from Products.ATContentTypes.interfaces.IATTopic import IATTopicSortCriterion
 from Products.ATContentTypes.types.criteria.ATBaseCriterion import ATBaseCriterion
 from Products.ATContentTypes.types.criteria.schemata import ATSortCriterionSchema
