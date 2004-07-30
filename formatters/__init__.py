203b47cff50848084caa91e78e3dfa06248ae060
davconvent <davconvent@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2004-07-30 09:28:59 +0000

    version 0.2 of ATBiblioList. Sorry, it is not compatible with the version 0.1 but it is for good reasons and should not happen again.

--- formatters/__init__.py
+++
@@ -1,7 +0,0 @@
-from MinimalFormat import MinimalFormat, manage_addMinimalFormat
-
-
-def initialize(context):
-    context.registerClass(MinimalFormat,
-                          constructors = (manage_addMinimalFormat,),
-                          ) 
