b4fb6a3269841c51d3f2c4e5f0c4608116ce42b4
k_vertigo <k_vertigo@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2004-09-12 07:27:19 +0000

    New repository initialized by cvs2svn.

--- doc/BibrefStyleExample/__init__.py
+++
@@ -1,6 +0,0 @@
-from BibrefStyleExample import BibrefStyleExample, manage_addBibrefStyleExample
-
-def initialize(context):
-    context.registerClass(BibrefStyleExample,
-                          constructors = (manage_addBibrefStyleExample,),
-                          )
