e18ac0698ab9785c61549781c5c6fc44673a96c6
panjunyong <panjunyong@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2005-01-10 18:21:30 +0000

    use CMFFormController and remove portal_form to make it compatible with Plone 2.1

--- Extensions/Install.py
+++ Extensions/Install.py
@@ -21,45 +21,6 @@ def changeVisibility(p, portal_type, action, value):
               a.visible=value
     tt[portal_type]._actions=actions
 
-def setupNavigation(p):
-    portal_form = getToolByName(p, 'portal_form')
-    portal_navigation = getToolByName(p, 'portal_navigation')
-
-    portal_form.setValidators('mpoll_slot' , ['mpoll_validate_vote'])
-    portal_form.setValidators('mpoll_poll' , ['mpoll_validate_vote'])
-    
-    # set up navigation properties
-    # Ploneboard
-    portal_navigation.addTransitionFor('default',
-                                       'mpoll_vote',
-                                       'failure',
-                                       'action:view')
-
-    portal_navigation.addTransitionFor('default',
-                                       'mpoll_vote',
-                                       'success',
-                                       'action:view')
-
-    portal_navigation.addTransitionFor('default',
-                                       'mpoll_slot',
-                                       'failure',
-                                       'action:view')
-
-    portal_navigation.addTransitionFor('default',
-                                       'mpoll_slot',
-                                       'success',
-                                       'script:mpoll_vote')
-
-    portal_navigation.addTransitionFor('default',
-                                       'mpoll_poll',
-                                       'success',
-                                       'script:mpoll_vote')
-
-    portal_navigation.addTransitionFor('default',
-                                       'mpoll_poll',
-                                       'failure',
-                                       'action:view')
-
 def install(self):
     out = StringIO()
 
@@ -71,8 +32,6 @@ def install(self):
                  listTypes(PROJECTNAME),
                  PROJECTNAME,
                  types_globals)
-
-    setupNavigation(self)
 
     print >> out, "Successfully installed %s." % PROJECTNAME
     return out.getvalue()
