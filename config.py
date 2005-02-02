c36ff9580aab0e40346292a2dee2e087d074395b
panjunyong <panjunyong@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2005-02-02 09:43:46 +0000

    remove some useless viewlets

--- config.py
+++ config.py
@@ -78,28 +78,12 @@ VIEWLETS = [
 'string:here/viewlets_folder_recent/macros/base_portlet',
 '', 'View', 'GL:folder',1 ),
 
-('latest_updates_viewlet2', 'Recent changes(2)',
-'string:here/viewlets_folder_recent/macros/inline_description',
-'', 'View', 'GL:folder',1 ),
-
-('latest_updates_viewlet3', 'Recent changes(3)',
-'string:here/viewlets_folder_recent/macros/separate_line_description',
-'', 'View', 'GL:folder',1 ),
-
 ('full_recent_changes', 'Folder changes',
 'string:here/viewlet_full_changes/macros/viewlet',
 '', 'View', 'GL:folder',1 ),
 
 ('folder_list_viewlet' , 'Folder listing',
 'string:here/viewlets_folder_listing/macros/base_portlet', 
-'', 'View', 'GL:folder', 1),
-
-('folder_list_viewlet2' , 'Folder listing(2)',
-'string:here/viewlets_folder_listing/macros/inline_description',
-'', 'View', 'GL:folder', 1),
-
-('folder_list_viewlet3' , 'Folder listing(3)',
-'string:here/viewlets_folder_listing/macros/separate_line_description',
 '', 'View', 'GL:folder', 1),
 
 ('portlet_calendar', 'Calendar', 
