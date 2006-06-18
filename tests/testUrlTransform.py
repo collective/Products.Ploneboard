93e4617845b91dd584d2a644904dc86b97d4a33b
wichert <wichert@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2006-06-18 16:57:14 +0000

    Add unittests for the URL transform

---
+++ tests/testUrlTransform.py
@@ -0,0 +1,61 @@
+#
+# Comment tests
+#
+
+import os, sys
+if __name__ == '__main__':
+    execfile(os.path.join(sys.path[0], 'framework.py'))
+
+import PloneboardTestCase
+from Products.Ploneboard.transforms.url_to_hyperlink import URLToHyperlink
+
+class MockTransformData:
+    def setData(self, value):
+        self.data=value
+
+
+class TestUrlTransform(PloneboardTestCase.PloneboardTestCase):
+    def runTest(self, testdata):
+        transform = URLToHyperlink()
+        data=MockTransformData()
+        for (input, answer) in testdata:
+            transform.convert(input, data)
+            self.failUnlessEqual(data.data, answer)
+
+    def testPlainText(self):
+        testdata = [
+                ("just a simple string",  "just a simple string"),
+                ("htXtp:bla invalid scheme", "htXtp:bla invalid scheme"),
+                ]
+        self.runTest(testdata)
+
+    def testPlainUrls(self):
+        testdata = [
+                ("http://simple.url/", '<a href="http://simple.url/">http://simple.url/</a>'),
+                ("https://simple.url/", '<a href="https://simple.url/">https://simple.url/</a>'),
+                ("telnet://simple.url/", '<a href="telnet://simple.url/">telnet://simple.url/</a>'),
+                ]
+        self.runTest(testdata)
+
+    def testUrlElements(self):
+        testdata = [
+                ("<telnet://simple.url/>", '<telnet://simple.url/>'),
+                ("< telnet://simple.url/>", '< telnet://simple.url/>'),
+                ("<telnet://simple.url />", '<telnet://simple.url />'),
+                ("http://change.this/ <telnet://simple.url/>", '<a href="http://change.this/">http://change.this/</a> <telnet://simple.url/>'),
+                ]
+        self.runTest(testdata)
+
+
+if __name__ == '__main__':
+    framework()
+else:
+    import unittest
+    def test_suite():
+        suite = unittest.TestSuite()
+        suite.addTest(unittest.makeSuite(TestUrlTransform))
+        
+        return suite
+
+
+
