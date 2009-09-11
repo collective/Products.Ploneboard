b8793807bbfbcd1b7c8446ab269b0a121767ec11
tesdal <tesdal@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-09-11 20:18:16 +0000

    Added view skeletons for board and forum.

--- Products/Ploneboard/browser/comment.py
+++ Products/Ploneboard/browser/comment.py
@@ -1,11 +1,7 @@
 """
 $Id$
 """
-from datetime import datetime
-from dateutil.parser import parse as dateparse
-import time
 from zope import interface
-from zope.i18n import translate
 from Acquisition import aq_base
 from DateTime.DateTime import DateTime
 from Products import Five
@@ -15,19 +11,9 @@ from Products.Ploneboard import permissions
 from Products.Ploneboard.batch import Batch
 from Products.Ploneboard.browser.interfaces import IConversationView
 from Products.Ploneboard.browser.interfaces import ICommentView
+from Products.Ploneboard.browser.utils import toPloneboardTime
 from Products.Ploneboard.utils import PloneboardMessageFactory as _
-from Products.CMFPlone import PloneMessageFactory as _plone
-from Products.CMFPlone import PloneLocalesMessageFactory as _locales
 
-class defer(object):
-    """Defer function call until actually used. Useful for date components in translations"""
-    def __init__(self, func, *args, **kwargs):
-        self.func = func
-        self.args = args
-        self.kwargs = kwargs
-
-    def __str__(self):
-        return self.func(*self.args, **self.kwargs)
 
 class CommentViewableView(Five.BrowserView):
     """Any view that might want to interact with comments should inherit
@@ -71,52 +57,7 @@ class CommentViewableView(Five.BrowserView):
 
     def toPloneboardTime(self, time_=None):
         """Return time formatted for Ploneboard"""
-        ploneboard_time=None
-        ts = getToolByName(self.context, 'translation_service')
-
-        format = '%Y;%m;%d;%w;%H;%M;%S'
-
-        # fallback formats, english
-        young_format_en = '%A %H:%M' 
-        old_format_en = '%B %d. %Y'
-
-
-        if not time_:
-            return 'Unknown date'
-
-        try:
-            if isinstance(time_, DateTime):
-                time_ = datetime.fromtimestamp(time_.timeTime())
-            else:
-                time_ = dateparse(str(time_))
-
-            (year, month, day, hours, minutes, seconds, wday, _, dst) = time_.timetuple()
-            translated_date_elements = { 'year'   : year
-                                       , 'month'  : defer(translate, _locales(ts.month_msgid(month)), context=self.request)
-                                       , 'day'    : day
-                                       , 'wday'   : defer(translate, _locales(ts.day_msgid((wday+1)%7)), context=self.request)
-                                       , 'hours'  : hours
-                                       , 'minutes': minutes
-                                       , 'seconds': seconds
-                                       }
- 
-            if time.time() - time.mktime(time_.timetuple()) < 604800: # 60*60*24*7
-                ploneboard_time = translate(_plone( 'young_date_format: ${wday} ${hours}:${minutes}'
-                                                  , default = unicode(time_.strftime(young_format_en))
-                                                  , mapping=translated_date_elements)
-                                            , context=self.request
-                                            )
-            else:
-                ploneboard_time = translate( _plone( 'old_date_format: ${year} ${month} ${day} ${hours}:${minutes}'
-                                                   , default = unicode(time_.strftime(old_format_en))
-                                                   , mapping = translated_date_elements)
-                                            , context=self.request
-                                            )
-
-        except IndexError:
-            pass 
-
-        return ploneboard_time
+        return toPloneboardTime(self.context, self.request, time_)
 
 
 class CommentView(CommentViewableView):
