f737dbf7e6d8ca9818b7eab1fd522b933b72eeb4
tesdal <tesdal@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-09-18 21:54:43 +0000

    Use indexer decorator instead of registerIndexableAttribute

--- Products/Ploneboard/catalog.py
+++ Products/Ploneboard/catalog.py
@@ -1,11 +1,6 @@
-from Products.CMFPlone.CatalogTool import registerIndexableAttribute
 from Products.Ploneboard.interfaces import IConversation
 
-def num_comments(object, portal, **kw):
-    conv = IConversation(object, None)
-    if conv is None:
-        return None
-    else:
-        return conv.getNumberOfComments()
-
-registerIndexableAttribute('num_comments', num_comments)
+from plone.indexer.decorator import indexer
+@indexer(IConversation)
+def num_comments(obj):
+    return obj.getNumberOfComments()
