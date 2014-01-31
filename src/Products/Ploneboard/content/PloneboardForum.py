3e26120fd8da949843dec5a6321de42b085ee3a0
Jens W. Klein <jens@bluedynamics.com>
2014-01-31 13:52:33 +0100

    some cleanup

--- src/Products/Ploneboard/content/PloneboardForum.py
+++ src/Products/Ploneboard/content/PloneboardForum.py
@@ -1,8 +1,9 @@
 from AccessControl import ClassSecurityInfo
 from Acquisition import aq_chain
-from Acquisition import aq_chainaq_inner
+from Acquisition import aq_inner
 from OFS.CopySupport import CopyContainer
 from OFS.Image import File
+from plone import api
 from Products.Archetypes.event import ObjectInitializedEvent
 from Products.Archetypes.public import BaseBTreeFolder
 from Products.Archetypes.public import BaseBTreeFolderSchema
@@ -19,8 +20,8 @@ from Products.Archetypes.public import Schema
 from Products.Archetypes.public import SelectionWidget
 from Products.Archetypes.public import TextAreaWidget
 from Products.Archetypes.public import TextField
-from Products.CMFCore.utils import getToolByName
-from Products.CMFPlone.interfaces import INonStructuralFolder as ZopeTwoINonStructuralFolder
+from Products.CMFPlone.interfaces import INonStructuralFolder \
+    as ZopeTwoINonStructuralFolder
 from Products.CMFPlone.interfaces.structure import INonStructuralFolder
 from Products.CMFPlone.utils import _createObjectByType, log_deprecated
 from Products.Ploneboard import utils
@@ -49,77 +50,94 @@ AttachmentSizes = IntDisplayList((
 
 schema = BaseBTreeFolderSchema + Schema((
     TextField('description',
-              searchable=1,
-              default_content_type='text/html',
-              default_output_type='text/plain',
-              widget=TextAreaWidget(
-                        description="Brief description of the forum topic.",
-                        description_msgid="help_description_forum",
-                        label="Description",
-                        label_msgid="label_description_forum",
-                        i18n_domain="ploneboard",
-                        rows=5)),
-
+        searchable=1,
+        default_content_type='text/html',
+        default_output_type='text/plain',
+        widget=TextAreaWidget(
+            description="Brief description of the forum topic.",
+            description_msgid="help_description_forum",
+            label="Description",
+            label_msgid="label_description_forum",
+            i18n_domain="ploneboard",
+            rows=5
+        )
+    ),
     LinesField('category',
-                 write_permission=ManageForum,
-                 vocabulary='getCategories',
-                 widget=MultiSelectionWidget(
-                            description="Select which category the forum should be listed under. A forum can exist in multiple categories, although using only one category is recommended.",
-                            description_msgid="help_category",
-                            condition="object/getCategories",
-                            label="Category",
-                            label_msgid="label_category",
-                            i18n_domain="ploneboard",
-                          )),
+         write_permission=ManageForum,
+         vocabulary='getCategories',
+         widget=MultiSelectionWidget(
+            description="Select which category the forum should be listed "
+                        "under. A forum can exist in multiple categories, "
+                        "although using only one category is recommended.",
+            description_msgid="help_category",
+            condition="object/getCategories",
+            label="Category",
+            label_msgid="label_category",
+            i18n_domain="ploneboard",
+          )
+    ),
     IntegerField('maxAttachments',
-                write_permission=ManageForum,
-                default=1,
-                widget=IntegerWidget(
-                         description="Select the maximum number of attachments per comment.",
-                         description_msgid="help_maxattachments",
-                         label="Maximum number of attachments",
-                         label_msgid="label_maxattachments",
-                         i18n_domain="ploneboard",
-                )),
+        write_permission=ManageForum,
+        default=1,
+        widget=IntegerWidget(
+             description="Select the maximum number of attachments per comment.",
+             description_msgid="help_maxattachments",
+             label="Maximum number of attachments",
+             label_msgid="label_maxattachments",
+             i18n_domain="ploneboard",
+        )
+    ),
     IntegerField('maxAttachmentSize',
-                write_permission=ManageForum,
-                vocabulary=AttachmentSizes,
-                default=100,
-                widget=SelectionWidget(
-                         description="Select the maximum size for attachments.",
-                         description_msgid="help_maxattachmentsize",
-                         label="Maximum attachment size",
-                         label_msgid="label_maxattachmentsize",
-                         i18n_domain="ploneboard",
-                )),
+        write_permission=ManageForum,
+        vocabulary=AttachmentSizes,
+        default=100,
+        widget=SelectionWidget(
+             description="Select the maximum size for attachments.",
+             description_msgid="help_maxattachmentsize",
+             label="Maximum attachment size",
+             label_msgid="label_maxattachmentsize",
+             i18n_domain="ploneboard",
+        )
+    ),
     BooleanField('allowEditComment',
         default=False,
         languageIndependent=0,
         widget=BooleanWidget(
             label=u'Allow users to edit their comments',
-            description=u'If selected, this will give users the ability to edit their own comments.',
+            description=u'If selected, this will give users the ability to '
+                        u'edit their own comments.',
             label_msgid='label_allow_edit_comment',
             description_msgid='help_allow_edit_comment',
             # Only show when no conversations exist
             condition="not:object/getNumberOfConversations|nothing",
             ),
     ),
-    BooleanField('showCaptcha',
-                 write_permission=ManageForum,
-                 default=False,
-                 widget=BooleanWidget(
-                         description=_(u'help_showcaptcha', default=u'Select if show or not captcha for anonymous (if recaptcha installed and configured).'),
-                         label=_(u'label_show_captcha', default=u"Show Captcha"),
-                )),
-    ))
+    BooleanField(
+        'showCaptcha',
+        write_permission=ManageForum,
+        default=False,
+        widget=BooleanWidget(
+            description=_(
+                u'help_showcaptcha',
+                default=u'Select if show or not captcha for anonymous (if '
+                        u'recaptcha installed and configured).'),
+            label=_(u'label_show_captcha', default=u"Show Captcha"),
+        )),
+))
 utils.finalizeSchema(schema)
 
 
 if not HAS_SIMPLEATTACHMENT:
     schema['maxAttachments'].mode = "r"
     schema['maxAttachments'].default = 0
-    schema['maxAttachments'].widget.visible = {'edit' : 'invisible', 'view' : 'invisible' }
-    schema['maxAttachmentSize'].widget.visible = {'edit' : 'invisible', 'view' : 'invisible' }
+    schema['maxAttachments'].widget.visible = {
+        'edit' : 'invisible',
+        'view' : 'invisible'
+    }
+    schema['maxAttachmentSize'].widget.visible = {
+        'edit' : 'invisible',
+        'view' : 'invisible'
+    }
 
 
 @implementer(IForum, INonStructuralFolder)
@@ -132,20 +150,20 @@ class PloneboardForum(BaseBTreeFolder):
 
     security = ClassSecurityInfo()
 
-    def getCatalog(self):
-        return getToolByName(self, 'portal_catalog')
+    def _get_catalog(self):
+        return api.portal.get_tool(name='portal_catalog')
 
-    security.declareProtected(ManageForum, 'edit')
+    @security.protected(ManageForum)
     def edit(self, **kwargs):
         """Alias for update()
         """
         self.update(**kwargs)
 
-    security.declarePublic('synContentValues')
+    @security.public
     def synContentValues(self):
         return (self.getConversations())
 
-    security.declareProtected(ViewBoard, 'getBoard')
+    @security.protected(ViewBoard)
     def getBoard(self):
         """Returns containing or nearest board."""
         # Try containment
@@ -157,7 +175,7 @@ class PloneboardForum(BaseBTreeFolder):
 
         return None
 
-    security.declareProtected(AddConversation, 'addConversation')
+    @security.protected(AddConversation)
     def addConversation(self, title, text=None, creator=None, files=None,
                         conversation_type='PloneboardConversation',
                         **kwargs):
@@ -182,7 +200,11 @@ class PloneboardForum(BaseBTreeFolder):
 
         event.notify(ObjectInitializedEvent(conv))
         if text is not None or files:
-            m = _createObjectByType('PloneboardComment', conv, conv.generateId(prefix=''))
+            m = _createObjectByType(
+                'PloneboardComment',
+                conv,
+                conv.generateId(prefix='')
+            )
 
             # XXX: There is some permission problem with AT write_permission
             # and using **kwargs in the _createObjectByType statement.
@@ -194,11 +216,16 @@ class PloneboardForum(BaseBTreeFolder):
                 m.setCreators([creator])
 
             # Create files in message
-            if files:
-                for file in files:
-                    # Get raw filedata, not persistent object with reference to tempstorage
-                    attachment = File(file.getId(), file.title_or_id(), str(file.data), file.getContentType())
-                    m.addAttachment(attachment)
+            for file in files or []:
+                # Get raw filedata, not persistent object with reference to
+                # tempstorage
+                attachment = File(
+                    file.getId(),
+                    file.title_or_id(),
+                    str(file.data),
+                    file.getContentType()
+                )
+                m.addAttachment(attachment)
 
             event.notify(ObjectInitializedEvent(m))
             m.reindexObject()
@@ -206,11 +233,11 @@ class PloneboardForum(BaseBTreeFolder):
         conv.reindexObject()
         return conv
 
-    security.declareProtected(ViewBoard, 'getConversation')
+    @security.protected(ViewBoard)
     def getConversation(self, conversation_id, default=None):
         """Returns the conversation with the given conversation id."""
         # return self._getOb(conversation_id, default)
-        catalog = self.getCatalog()
+        catalog = self._get_catalog()
         conversations = catalog(
                 object_provides=IConversation.__identifier__,
                 getId=conversation_id,
@@ -220,75 +247,91 @@ class PloneboardForum(BaseBTreeFolder):
         else:
             return None
 
-    security.declareProtected(ManageForum, 'removeConversation')
+    @security.protected(ManageForum)
     def removeConversation(self, conversation_id):
-        """Removes a conversation with the given conversation id from the forum."""
+        """Removes a conversation with the given conversation id from the
+        forum.
+        """
         self._delObject(conversation_id)
 
-    security.declareProtected(ViewBoard, 'getConversations')
+    @security.protected(ViewBoard)
     def getConversations(self, limit=20, offset=0):
         """Returns conversations."""
-        log_deprecated("Products.Ploneboard.content.PloneboardForum.PloneboardForum.getConversations is deprecated in favor of Products.Ploneboard.browser.forum.ForumView.getConversations")
-        catalog = self.getCatalog()
-        return [f.getObject() for f in \
-                catalog(object_provides=IConversation.__identifier__,
-                        sort_on='modified',
-                        sort_order='reverse',
-                        sort_limit=(offset + limit),
-                        path='/'.join(self.getPhysicalPath()))[offset:offset + limit]]
+        log_deprecated(
+            'Products.Ploneboard.content.PloneboardForum.'
+            'PloneboardForum.getConversations is deprecated in favor of '
+            'Products.Ploneboard.browser.forum.ForumView.getConversations'
+        )
+        catalog = self._get_catalog()
+        brains = catalog(
+            object_provides=IConversation.__identifier__,
+            sort_on='modified',
+            sort_order='reverse',
+            sort_limit=(offset + limit),
+            path='/'.join(self.getPhysicalPath())
+        )
+        return [f.getObject() for f in brains[offset:offset + limit]]
 
-    security.declareProtected(ViewBoard, 'getNumberOfConversations')
+
+    @security.protected(ViewBoard)
     def getNumberOfConversations(self):
         """Returns the number of conversations in this forum."""
-        log_deprecated("Products.Ploneboard.content.PloneboardForum.PloneboardForum.getNumberOfConversations is deprecated in favor of Products.Ploneboard.browser.forum.ForumView.getNumberOfConversations")
-        return len(self.getCatalog()(
-            object_provides=IConversation.__identifier__,
-            path='/'.join(self.getPhysicalPath())))
+        log_deprecated("Products.Ploneboard.content.PloneboardForum."
+                       "PloneboardForum.getNumberOfConversations is "
+                       "deprecated in favor of Products.Ploneboard.browser."
+                       "forum.ForumView.getNumberOfConversations"
+        )
+        return len(
+            self._get_catalog()(
+                object_provides=IConversation.__identifier__,
+                path='/'.join(self.getPhysicalPath())
+            )
+        )
 
-    security.declareProtected(ViewBoard, 'getNumberOfComments')
+    @security.protected(ViewBoard)
     def getNumberOfComments(self):
         """Returns the number of comments to this forum."""
-        log_deprecated("Products.Ploneboard.content.PloneboardForum.PloneboardForum.getNumberOfComments is deprecated in favor of Products.Ploneboard.browser.forum.ForumView.getNumberOfComments")
-        return len(self.getCatalog()(
+        log_deprecated(
+            "Products.Ploneboard.content.PloneboardForum."
+            "PloneboardForum.getNumberOfComments is deprecated in favor of "
+            "Products.Ploneboard.browser.forum.ForumView.getNumberOfComments"
+        )
+        return len(self._get_catalog()(
             object_provides='Products.Ploneboard.interfaces.IComment',
             path='/'.join(self.getPhysicalPath())))
 
-    security.declareProtected(ViewBoard, 'getLastConversation')
+    @security.protected(ViewBoard)
     def getLastConversation(self):
         """
         Returns the last conversation.
         """
         # XXX Is Created or Modified the most interesting part?
-        res = self.getCatalog()(
+        res = self._get_catalog()(
                 object_provides=IConversation.__identifier__,
                 sort_on='created', sort_order='reverse', sort_limit=1,
                 path='/'.join(self.getPhysicalPath()))
         if res:
             return res[0].getObject()
-        else:
-            return None
 
-    security.declareProtected(ViewBoard, 'getLastCommentDate')
+    @security.protected(ViewBoard)
     def getLastCommentDate(self):
         """
         Returns a DateTime corresponding to the timestamp of the last comment
         for the forum.
         """
-        res = self.getCatalog()(
+        res = self._get_catalog()(
             object_provides='Products.Ploneboard.interfaces.IComment',
             sort_on='created', sort_order='reverse', sort_limit=1,
             path='/'.join(self.getPhysicalPath()))
         if res:
             return res[0].created
-        else:
-            return None
 
-    security.declareProtected(ViewBoard, 'getLastCommentAuthor')
+    @security.protected(ViewBoard)
     def getLastCommentAuthor(self):
         """
         Returns the name of the author of the last comment.
         """
-        res = self.getCatalog()(
+        res = self._get_catalog()(
                 object_provides='Products.Ploneboard.interfaces.IComment',
                 sort_on='created', sort_order='reverse', sort_limit=1,
                 path='/'.join(self.getPhysicalPath()))
@@ -298,7 +341,7 @@ class PloneboardForum(BaseBTreeFolder):
             return None
 
     # Vocabularies
-    security.declareProtected(ViewBoard, 'getCategories')
+    @security.protected(ViewBoard)
     def getCategories(self):
         value = []
         board = self.getBoard()
@@ -309,7 +352,7 @@ class PloneboardForum(BaseBTreeFolder):
         value.sort()
         return DisplayList(value)
 
-    security.declareProtected(ViewBoard, 'getAttachmentSizes')
+    @security.protected(ViewBoard)
     def getAttachmentSizes(self):
         voc = DisplayList()
         voc.add('10', '10 kilobyte')
@@ -317,12 +360,11 @@ class PloneboardForum(BaseBTreeFolder):
         voc.add('1000', '1 megabyte')
         voc.add('10000', '10 megabyte')
         voc.add('-1', 'unlimited')
-
         return voc
 
-    security.declarePublic('getConversationBatchSize')
+    @security.public
     def getConversationBatchSize(self):
-        pb_tool = getToolByName(self, 'portal_ploneboard')
+        pb_tool = api.portal.get_tool(name='portal_ploneboard')
         prop_name = "%s_ConversationBatchSize" % self.getId()
         if pb_tool.hasProperty(prop_name):
             return pb_tool.getProperty(prop_name)
@@ -335,7 +377,7 @@ class PloneboardForum(BaseBTreeFolder):
     ############################################################################
     # Folder methods, indexes and such
 
-    security.declareProtected(MoveConversation, 'manage_pasteObjects')
+    @security.protected(MoveConversation)
     def manage_pasteObjects(self, cp):
         """ move another conversation """
         CopyContainer.manage_pasteObjects(self, cp)
